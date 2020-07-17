0eb3c2bb3e490b7f0d945a8d9669d79d329e2bd5
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-16 19:54:51 -0500

    broke up reducer into functions that handle different parts of the app state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,41 @@
 import {ADD_TO_CART, REMOVE_FROM_CART, CHANGE_CATEGORY} from "../constants/actionTypes";
 
-let initialState = {
-    activeCategory: 'food',
-    products: [
-        {id: 1, name: 'pizza', cost: 10, category: 'food'},
-        {id: 2, name: 'hamburger', cost: 5, category: 'food'},
-        {id: 3, name: 'tv', cost: 599, category: 'electronics'},
-        {id: 4, name: 'phone', cost: 799, category: 'electronics'},
-    ],
-    shoppingCart: [
-
-    ]
-};
-
-function reducer(state = initialState, action) {
+let initial1 = 'food'
+function activeCategory(state = initial1, action) {
     switch(action.type) {
-    case ADD_TO_CART:
-        return {
-            ...state,
-            shoppingCart: [...state.shoppingCart, action.payload]
-        };
-    case REMOVE_FROM_CART:
-        return {
-            ...state,
-            shoppingCart: state.shoppingCart.filter(productId => productId != action.payload)
-        };
     case CHANGE_CATEGORY:
-        return {
-            ...state,
-            activeCategory: action.payload,
-        };
+        return action.payload;
     default:
         return state;
+    }
+}
+
+let initial2 = [
+    {id: 1, name: 'pizza', cost: 10, category: 'food'},
+    {id: 2, name: 'hamburger', cost: 5, category: 'food'},
+    {id: 3, name: 'tv', cost: 599, category: 'electronics'},
+    {id: 4, name: 'phone', cost: 799, category: 'electronics'},
+];
+function products(state = initial2, action) {
+    return state;
+}
+
+let initial3 = [];
+function shoppingCart(state = initial3, action) {
+    switch(action.type) {
+    case ADD_TO_CART:
+        return [...state, action.payload];
+    case REMOVE_FROM_CART:
+        return state.filter(productId => productId != action.payload);
+    default:
+        return state;
+    }
+}
+
+function reducer(state = initialState, action) {
+    return {
+        activeCategory: activeCategory(state,action),
+        products: products(state,action),
+        shoppingCart: shoppingCart(state,action),
     }
 }
